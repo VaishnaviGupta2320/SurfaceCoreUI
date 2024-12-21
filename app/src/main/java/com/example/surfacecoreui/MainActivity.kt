70b65ccc54fc689a141cfa67d11781d8aa935131
Vaishnavi Gupta <150041663+VaishnaviGupta2320@users.noreply.github.com>
2024-12-21 20:56:42 +0530

    surface core ui syntax and functionality

--- app/src/main/java/com/example/surfacecoreui/MainActivity.kt
+++ app/src/main/java/com/example/surfacecoreui/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.surfacecoreui
 
+import android.inputmethodservice.Keyboard.Row
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -18,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.surfacecoreui.ui.theme.SurfaceCoreUiTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,12 +41,41 @@ fun MainScreen(){
     Surface(color = Color.DarkGray,
         modifier = Modifier.fillMaxSize())
     {
-Surface(color = Color.Magenta, modifier = Modifier.wrapContentSize(align = Alignment.TopCenter)) {
-    Text(text = "Wrapped Content",
-       // modifier = Modifier.wrapContentSize(),
-        style = MaterialTheme.typography.headlineLarge)
-}
-    }
+       Row {
+           Horizontalcolorbar(Color.Magenta)
+           Horizontalcolorbar(Color.Green)
+           Horizontalcolorbar(Color.Blue)
+           Horizontalcolorbar(Color.Cyan)
+           Horizontalcolorbar(Color.Yellow)
+//           Surface(
+//               color = Color.Magenta,
+//               modifier = Modifier.height(600.dp).width(100.dp)
+//           ){ }
+//           Surface(
+//               color = Color.Green,
+//               modifier = Modifier.height(600.dp).width(100.dp)
+//           ){ }
+//           Surface(
+//               color = Color.Blue,
+//               modifier = Modifier.height(600.dp).width(100.dp)
+//           ){ }
+
+       }
+
+//Surface(color = Color.Magenta, modifier = Modifier.wrapContentSize(align = Alignment.TopCenter)) {
+                //Text(text = "Wrapped Content",
+                // modifier = Modifier.wrapContentSize(),
+                //  style = MaterialTheme.typography.headlineLarge)
+            }
+        }
+
+@Composable
+fun Horizontalcolorbar(color: Color)
+{
+    Surface(
+        color = color,
+        modifier = Modifier.height(600.dp).width(80.dp)
+    ){ }
 }
 
 
